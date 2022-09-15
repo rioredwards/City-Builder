99d5a816bc93b4ab151c6555d5aefa31b537ef68
Rio Edwards <rioredwards@gmail.com>
2022-09-15 14:23:22 -0700

    Add classList to city based on climate/architecture

--- app.js
+++ app.js
@@ -3,6 +3,11 @@
 /* Get DOM Elements */
 const nameInput = document.getElementById('name-input');
 const nameDisplay = document.getElementById('name-display');
+const climateSelect = document.getElementById('climate-select');
+const climateDisplay = document.getElementById('climate-image');
+const architectureSelect = document.getElementById('architecture-select');
+const architectureDisplay = document.getElementById('architecture-image');
+const cityArticle = document.getElementById('city-article');
 
 /* State */
 let city = {
@@ -23,9 +28,23 @@ nameInput.addEventListener('input', () => {
     displayCity();
 });
 
+climateSelect.addEventListener('change', () => {
+    city.climate = climateSelect.value;
+    displayCity();
+});
+
+architectureSelect.addEventListener('change', () => {
+    city.architecture = architectureSelect.value;
+    displayCity();
+});
+
 /* Display Functions */
 function displayCity() {
     nameDisplay.textContent = city.name;
+    cityArticle.classList.value = '';
+    cityArticle.classList.add(city.climate, city.architecture);
+    climateDisplay.src = './assets/climate/' + city.climate + '.jpg';
+    architectureDisplay.src = './assets/architecture/' + city.architecture + '.jpg';
 }
 
 // (don't forget to call any display functions you want to run on page load!)
