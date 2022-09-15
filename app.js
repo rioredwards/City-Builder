98eb5ea9f9000b0d81b602d67599b11578f9b5e6
Rio Edwards <rioredwards@gmail.com>
2022-09-15 10:11:15 -0700

    Start on Javascript. Add city state object and displayCity fucntion

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 /* Imports */
 
 /* Get DOM Elements */
+const nameInput = document.getElementById('name-input');
+const nameDisplay = document.getElementById('name-display');
 
 /* State */
+let city = {
+    name: 'Cool Town',
+    climate: 'desert',
+    architecture: 'modern',
+    attractions: [],
+};
 
 /* Events */
+function pageLoad() {
+    displayCity();
+}
 
 /* Display Functions */
+function displayCity() {
+    nameDisplay.textContent = city.name;
+}
 
 // (don't forget to call any display functions you want to run on page load!)
+pageLoad();
